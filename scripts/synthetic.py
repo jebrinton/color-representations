cdf7e75a9e8b7c081fb64fb2e6a84bef6f8c93fd
Jacob Brinton <jbrin@bu.edu>
2025-10-29 14:28:07 -0400

    Added code to generate dataset. To do: make dataloader

--- scripts/synthetic.py
+++ scripts/synthetic.py
@@ -1,4 +1,12 @@
-from datetime import datetime, timezone
-print("Hola mundo")
+import json
 
-print(datetime.now(timezone.utc))
+with open("data/synthetic.json", "r") as f:
+    data = json.load(f)
+
+print(data)
+
+for template in data["templates"]:
+    for object in data["objects"]:
+        for color in data["colors"]:
+            for number in data["numbers"]:
+                print(template.format(object=object, color=color, number=number))
